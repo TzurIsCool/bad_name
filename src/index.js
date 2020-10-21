9d813ab2e0eed77e75e7a4bdccbc21a451bdda53
Andrew Rivard <andrivard4@icloud.com>
2020-10-21 17:15:51 -0400

    I think its fixed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React>
-    <h1>AHHH</h1>
-    <p>This is our shitty website</p>
-  </React>,
+  <React.StrictMode>
+    <div>
+      <h1>AHHH</h1>
+      <p>This is our shitty website</p>
+    </div>
+  </React.StrictMode>,
   document.getElementById('root')
 );
